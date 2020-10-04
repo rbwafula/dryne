7a05929ab6c6c12c8dccd8d59b132a0c29c29a48
Robert Wafula <rbwafula@gmail.com>
2020-10-04 14:19:29 +0300

    v0.0.4

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,11 @@
 // Select active page
-var path =  window.location.pathname.split("/").pop();
-var page = "ul#mainNav > li > a#" + path;
-$(page).addClass('active');
-
+var path = window.location.pathname.split("/").pop();
+if (path != "") {
+	var page = "ul#mainNav > li > a#" + path;
+	$(page).addClass('active');
+} else {
+	if (window.location.host == "drynegroup.com" || window.location.host == "www.drynegroup.com" || window.location.host == "localhost") {
+		var page = "ul#mainNav > li > a#home";
+		$(page).addClass('active');
+	}
+}
